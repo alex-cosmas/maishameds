672cd3b01782416d94abcb933b0598ae880ac06d
Juma Allan <allanjuma@gmail.com>
2020-11-28 01:25:58 +0300

    added test sample data

---
+++ app/src/test/java/org/maishameds/data/sample/SampleData.kt
@@ -0,0 +1,18 @@
+package org.maishameds.data.sample
+
+import org.maishameds.data.model.Post
+
+val testPosts = listOf(
+    Post(
+        0,
+        1,
+        "qui est esse",
+        "est rerum tempore vitae\\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\\nqui aperiam non debitis possimus qui neque nisi nulla"
+    ),
+    Post(
+        0,
+        2,
+        "eum et est occaecati",
+        "ullam et saepe reiciendis voluptatem adipisci\\nsit amet autem assumenda provident rerum culpa\\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\\nquis sunt voluptatem rerum illo velit"
+    )
+)
