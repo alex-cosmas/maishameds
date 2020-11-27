c7812b8c6887e46d01ab416d9b3b089e928c80a1
Juma Allan <allanjuma@gmail.com>
2020-11-28 01:41:26 +0300

    added fake post list

---
+++ app/src/androidTest/java/org/maishameds/fake/fakes.kt
@@ -0,0 +1,12 @@
+package org.maishameds.fake
+
+import org.maishameds.data.model.Post
+
+val fakePost = listOf(
+    Post(
+        0,
+        1,
+        "quas fugiat ut perspiciatis vero provident",
+        "eum non blanditiis soluta porro quibusdam voluptas\\nvel voluptatem qui placeat dolores qui velit aut\\nvel inventore aut cumque culpa explicabo aliquid at\\nperspiciatis est et voluptatem dignissimos dolor itaque sit nam"
+    )
+)
