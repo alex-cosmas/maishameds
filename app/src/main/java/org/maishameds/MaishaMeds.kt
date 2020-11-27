1ffbfe5a18e107977c3161661131bd780b8fff7a
Juma Allan <allanjuma@gmail.com>
2020-11-27 22:47:47 +0300

    setting up base application

---
+++ app/src/main/java/org/maishameds/MaishaMeds.kt
@@ -0,0 +1,55 @@
+package org.maishameds
+
+import android.app.Application
+import org.jetbrains.annotations.NotNull
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.error.KoinAppAlreadyStartedException
+import org.koin.core.logger.Level
+import org.koin.core.module.Module
+import org.maishameds.core.di.coreModules
+import org.maishameds.core.utils.CrashlyticsTree
+import org.maishameds.di.appModules
+import timber.log.Timber
+import javax.annotation.Nullable
+
+open class MaishaMeds : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initKoin()
+        initTimber()
+    }
+
+    private fun initKoin() {
+        try {
+            startKoin {
+                androidLogger(Level.ERROR)
+                androidContext(applicationContext)
+                val modules = mutableListOf<Module>().apply {
+                    addAll(coreModules)
+                    addAll(appModules)
+                }
+                modules(modules)
+            }
+        } catch (error: KoinAppAlreadyStartedException) {
+            Timber.e(error.localizedMessage)
+        }
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(object : Timber.DebugTree() {
+                @Nullable
+                override fun createStackElementTag(@NotNull element: StackTraceElement): String? {
+                    return super.createStackElementTag(element) + ":" + element.lineNumber
+                }
+            })
+        } else {
+            Timber.plant(CrashlyticsTree())
+        }
+    }
+
+}
